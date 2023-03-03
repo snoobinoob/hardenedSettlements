a0c80ed2089de93a8dac07de4b9d87f13c074bb0
Snoobinoob <cheeseycheese99@gmail.com>
2023-03-02 21:12:54 -0500

    Initial Commit

---
+++ src/main/java/antiBomb/AntiBomb.java
@@ -0,0 +1,11 @@
+package antiBomb;
+
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class AntiBomb {
+
+    public void init() {
+        System.out.println("Hello world from Anti Bomb!");
+    }
+}
