8f481f142979c526b070c960578c82bfa385d1cb
Snoobinoob <cheeseycheese99@gmail.com>
2023-03-16 17:43:11 -0400

    Update for game version 0.21.29

---
+++ src/main/java/hardenedSettlements/patch/GameTilePatch.java
@@ -0,0 +1,27 @@
+package hardenedSettlements.patch;
+
+import hardenedSettlements.extraSettlement.ExtraSettlementLevelData;
+import necesse.engine.modLoader.annotations.ModMethodPatch;
+import necesse.engine.network.server.ServerClient;
+import necesse.level.gameTile.GameTile;
+import necesse.level.maps.Level;
+import net.bytebuddy.asm.Advice;
+
+public class GameTilePatch {
+    @ModMethodPatch(target = GameTile.class, name = "doExplosionDamage", arguments = {Level.class,
+            int.class, int.class, int.class, int.class, ServerClient.class})
+    public static class DoExplosionDamagePatch {
+        @Advice.OnMethodEnter(skipOn = Advice.OnNonDefaultValue.class)
+        public static boolean onExit(@Advice.AllArguments Object[] args) {
+            Level level = (Level) args[0];
+            int x = (int) args[1];
+            int y = (int) args[2];
+            ExtraSettlementLevelData data = ExtraSettlementLevelData.getData(level);
+            if (level.settlementLayer.isActive() && !data.doExplosionDamage
+                    && data.getDefendZone().containsTile(x, y)) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
